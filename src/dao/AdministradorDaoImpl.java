0df8a5f0506850c2565ae124a2740bbb35be8185
fbio_ <fbio_henrique15@hotmail.com>
2017-08-15 09:25:06 -0300

    Tetse

--- src/dao/AdministradorDaoImpl.java
+++ src/dao/AdministradorDaoImpl.java
@@ -5,6 +5,7 @@ import model.Administrador;
 public class AdministradorDaoImpl extends GenericDaoImpl<Administrador, Integer>{
 	 
 	public AdministradorDaoImpl() { 
+		
 	        super(Administrador.class); 
 	 } 
 }
